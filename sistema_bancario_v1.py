78bf610aa3249fe8f36d39768542b45047ba0478
Claudino Neto <115763601+Zeromil2@users.noreply.github.com>
2024-05-16 18:34:15 -0300

    organizando o código do sistema_bancario_v1.py

--- sistema_bancario_v1.py
+++ sistema_bancario_v1.py
@@ -13,7 +13,9 @@ menu = '''
 => '''
 opcao = 0 # escolha que o usuário irá fazer
 while opcao != 4:
+
     opcao = int(input(menu))
+
     if opcao == 1:
         valor = int(input('Digite o valor (em R$): '))
         while valor < 0:
@@ -22,11 +24,15 @@ while opcao != 4:
         saldo += valor
         extrato += f'''Foi depositado o valor de R${valor} em sua conta.
 Saldo atual: R${saldo}\n'''
+
     elif opcao == 2:
+
         if saques_realizados == 3:
             print("Você já realizou o número máximo de saques. Tente outra opção.")
+
         else:
             valor = int(input('Digite o valor (em R$): '))
+
             while valor > limite_valor_por_saque or valor > saldo or valor < 0:
                 print("O valor digitado não é válido. Tente novamente.")
                 valor = int(input('Digite o valor (em R$): '))
@@ -34,10 +40,13 @@ Saldo atual: R${saldo}\n'''
             extrato += f'''Foi sacado o valor de R${valor} em sua conta.
 Saldo atual: R${saldo}'''
             saques_realizados += 1
+
     elif opcao == 3:
         print("EXTRATO".center(15, "="))
         print(extrato)
+
     elif opcao == 4:
         print("Saindo do sistema...")
+
     else:
         print("Opção inválida. Tente novamente.")
