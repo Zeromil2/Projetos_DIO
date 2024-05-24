4c00f4277ae7d5fe3eea320aa0be4b2b948eed23
Claudino Neto <115763601+Zeromil2@users.noreply.github.com>
2024-05-24 13:23:50 -0300

    Conceitos de encapsulamento (variável pública e privada), assim como exemplos de usos de properties

---
+++ Encapsulamento/encapsulamento.py
@@ -0,0 +1,21 @@
+class Conta:
+    def __init__(self, num_agencia, saldo=0):
+        self._saldo = saldo
+        self.num_agencia = num_agencia
+
+    def depositar(self, valor):
+        # ...
+        self._saldo += valor
+
+    def sacar(self, valor):
+        # ...
+        self._saldo -= valor
+
+    def mostrar_saldo(self):
+        # ...
+        return self._saldo
+
+conta = Conta('0001')
+conta.depositar(100)
+print(conta.num_agencia)
+print(conta.mostrar_saldo())
